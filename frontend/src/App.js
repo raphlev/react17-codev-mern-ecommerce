4cc47d57757bbef336cfb334363c024cf89da54e
raphlev <raphleveque@hotmail.fr>
2021-07-23 19:46:13 +0200

    Add Lazy Loading

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,15 @@
-import React, { useEffect } from 'react'
-import { Route } from 'react-router-dom'
+import React, {Suspense, useEffect } from 'react'
+import { Route, Switch } from 'react-router-dom'
 import ReactNotification from 'react-notifications-component'
 import styled from 'styled-components'
 import 'react-notifications-component/dist/theme.css'
 import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
 import 'react-toastify/dist/ReactToastify.css'
-
 import HomeScreen from './screens/HomeScreen'
-import ProductDetailsScreen from './screens/ProductDetailsScreen'
-import Cart from './screens/CartScreen'
-import LoginScreen from './screens/LoginScreen'
-import RegisterScreen from './screens/RegisterScreen'
 import { ToastContainer } from 'react-toastify'
-import ProfileScreen from './screens/ProfileScreen'
-import ShippingScreen from './screens/ShippingScreen'
 import Footer from './components/Footer'
 import Navigation from './components/Navigation'
-import PaymentScreen from './screens/PaymentScreen'
-import PlaceOrderScreen from './screens/PlaceOrderScreen'
-import OrderScreen from './screens/OrderScreen'
+import LoadingSpinner from './components/Spinner/LoadingSpinner'
 
 const Grid = styled.div`
   position: relative;
@@ -29,9 +20,27 @@ const Grid = styled.div`
   grid-template-columns: 1fr;
 `
 
+const Centered = styled.div`
+  margin: 3rem auto;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  `
+
 const Main = styled.main`
   grid-area: main;
 `
+
+const ProductDetailsScreen = React.lazy(() => import('./screens/ProductDetailsScreen'));
+const Cart  = React.lazy(() => import( './screens/CartScreen'));
+const ShippingScreen = React.lazy(() => import( './screens/ShippingScreen'));
+const PaymentScreen  = React.lazy(() => import( './screens/PaymentScreen'));
+const PlaceOrderScreen  = React.lazy(() => import( './screens/PlaceOrderScreen'));
+const OrderScreen  = React.lazy(() => import( './screens/OrderScreen'));
+const LoginScreen  = React.lazy(() => import( './screens/LoginScreen'));
+const RegisterScreen  = React.lazy(() => import( './screens/RegisterScreen'));
+const ProfileScreen  = React.lazy(() => import( './screens/ProfileScreen'));
 
 function App() {
   useEffect(() => {
@@ -44,16 +53,27 @@ function App() {
       <Grid>
         <Navigation />
         <Main>
-          <Route path='/products/:id' component={ProductDetailsScreen} />
-          <Route path='/cart/:id?' component={Cart} />
-          <Route path='/shipping' component={ShippingScreen} />
-          <Route path='/payment' component={PaymentScreen} />
-          <Route path='/placeorder' component={PlaceOrderScreen} />
-          <Route path='/orders/:id' component={OrderScreen} />
-          <Route path='/login' component={LoginScreen} />
-          <Route path='/register' component={RegisterScreen} />
-          <Route path='/profile' component={ProfileScreen} />
-          <Route exact path='/' component={HomeScreen} />
+        <Suspense
+        fallback={
+          <Centered>
+            <LoadingSpinner />
+          </Centered>
+        }
+        >
+          <Switch>
+            <Route path='/products/:id'component={ProductDetailsScreen} />
+            <Route path='/cart/:id?' component={Cart} />
+            <Route path='/shipping' component={ShippingScreen} />
+            <Route path='/payment' component={PaymentScreen} />
+            <Route path='/placeorder' component={PlaceOrderScreen} />
+            <Route path='/orders/:id' component={OrderScreen} />
+            <Route path='/login' component={LoginScreen} />
+            <Route path='/register' component={RegisterScreen} />
+            <Route path='/profile' component={ProfileScreen} />
+            <Route exact path='/' component={HomeScreen} />
+          </Switch>
+        </Suspense>
+          
         </Main>
         <Footer />
       </Grid>
---
+++ frontend/src/components/Spinner/LoadingSpinner.js
@@ -0,0 +1,8 @@
+import classes from './LoadingSpinner.module.css';
+
+const LoadingSpinner = () => {
+  // console.log("LoadingSpinner is called ...")
+  return <div className={classes.spinner}></div>;
+}
+
+export default LoadingSpinner;
